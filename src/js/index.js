37f421606a2e77f4d17e30c5481b23bd303fcede
Alexander <snarsky93@gmail.com>
2020-04-11 20:03:15 +0300

    feat: add basic config of task

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+import '../css/style.css';
+import '../css/style.scss';
+import {
+  moduleOne
+} from './moduleOne';
+import CARDS from './cards';
+
+const helloArr = require('./moduleOne.js');
+
+
+
+class TestClass {
+  constructor() {
+    const msg = "Using ES2015+ syntax";
+    console.log(msg);
+  }
+}
+
+const test = new TestClass();
+
+
+// Пример массива
+console.log(helloArr);
+
+/* пример подключения модуля*/
+let mod = moduleOne(2, 3);
+
+console.log(mod);
